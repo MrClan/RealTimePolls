f68231ab93b847c7ac75ad4265bf1c7a6b227192
CherutoMercy <m.thirty100@gmail.com>
2017-06-01 15:00:00 +0300

    add routes

---
+++ index.js
@@ -0,0 +1,24 @@
+var express    = require('express');
+var app        = express();
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var path = require('path');
+
+// set static folder
+app.use(express.static(__dirname + '/assets'));
+app.use(express.static(path.join(__dirname, 'views')));
+
+
+//Create EJS Engine view
+app.set('view engine', 'html');
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+var routes = require('./routes/index.js');
+
+
+app.listen(4000, () => {
+    console.log('Awesome server running on port 4000');
+});
+
+module.exports = app;
