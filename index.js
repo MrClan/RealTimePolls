0b57f6ebfa6ea54374b482d066791d125699ccfc
CherutoMercy <m.thirty100@gmail.com>
2017-06-01 18:58:56 +0300

    add passport files

--- index.js
+++ index.js
@@ -3,10 +3,48 @@ var app = express();
 var ejs = require('ejs');
 var engine = require('ejs-mate');
 var path = require('path');
+var flash = require('connect-flash');
+var mongoose= require('mongoose');
+var bodyParser=require('body-parser');
+var fs = require('fs');
+var multer = require('multer');
+var morgan       = require('morgan');
+var session = require('express-session');
+var MongoStore       =  require('connect-mongo')(session);
+var expressValidator = require('express-validator');
+var passport = require('passport');
+var cookieParser = require('cookie-parser');
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+const db      ='mongodb://localhost:27017/polls';
+mongoose.connect(db);
 
 // set static folder
 app.use(express.static(__dirname + '/assets'));
 app.use(express.static(path.join(__dirname, 'views')));
+
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(expressValidator()); 
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use(morgan('dev'));
+app.use(session({
+    secret: 'anything',
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({
+        url: db,
+        touchAfter: 24 * 3600
+    })
+  }));
 
 
 //Create EJS Engine view
@@ -14,10 +52,26 @@ app.set('view engine', 'html');
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
+require('./config/passport')(passport);
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(flash());
+
+app.use((req,res,next)=>{
+  res.locals.user   = req.user;
+  res.locals.message = req.flash();
+  next();
+});
+
+
 var routes = require('./routes/index.js');
 app.use(routes);
 
-app.listen(4000, () => {
+
+server.listen(4000, () => {
     console.log('Awesome server running on port 4000');
 });
 
