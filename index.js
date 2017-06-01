0e8a4d325bb8290e14acd9f9207766d4ce43f03c
Danielotieno <danielotieno.ke@gmail.com>
2017-06-01 16:39:06 +0300

    update index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var express    = require('express');
-var app        = express();
+var express = require('express');
+var app = express();
 var ejs = require('ejs');
 var engine = require('ejs-mate');
 var path = require('path');
